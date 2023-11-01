d1012da5e236cfc4ea866dd3bea0a9f988e7126e
Anton Mikaskin <garrakombo@gmail.com>
2023-11-01 20:42:34 +0300

    update config

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -3,4 +3,11 @@ vim.fn.sign_define("DiagnosticSignWarn", {text = " ", texthl = "DiagnosticSig
 vim.fn.sign_define("DiagnosticSignInfo", {text = " ", texthl = "DiagnosticSignInfo"})
 vim.fn.sign_define("DiagnosticSignHint", {text = "󰌵", texthl = "DiagnosticSignHint"})
 
-require('neo-tree').setup({})
+require("neo-tree").setup({
+    window = {
+      mappings = {
+        ["l"] = "open",
+        ["h"] = "close_node"
+      }
+    }
+  })
